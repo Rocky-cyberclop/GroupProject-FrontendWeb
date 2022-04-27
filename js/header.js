3c6d4bd185adeb7c98b02adbb573f64ce0c961ef
Rocky <rockyoperation@gmail.com>
2022-04-27 21:49:51 +0700

    change header and complete responsive

--- js/header.js
+++ js/header.js
@@ -2,11 +2,42 @@
 
 window.addEventListener('scroll', function(){
     var header = document.getElementsByTagName('header');
+    
     if(this.window.scrollY>0){
-        header[0].classList.replace('container', 'sticky');
+        if(header[0].classList.contains('container')){
+            header[0].classList.replace('container', 'sticky');
+        }
+        else{
+            header[0].classList.add('sticky');
+        }
+        
     }
     else{
-        header[0].classList.replace('sticky', 'container');
+        if(window.innerWidth<860){
+            header[0].classList.remove('sticky');
+        }
+        else{
+            header[0].classList.replace('sticky', 'container');
+        }
+        
     }
 });
 
+
+var nav = document.getElementsByTagName('nav');
+var a_nav = document.getElementsByClassName('nav__item__link');
+var header  = document.getElementsByTagName('header');
+if(window.innerWidth<860){
+    nav[0].children[3].classList.replace('display--flex', 'display--block');
+    header[0].classList.remove('container');
+    for(var i=0; i<a_nav.length; i++){
+        a_nav[i].classList.add('text--center');
+    }
+}
+else{
+    nav[0].children[3].classList.replace('display--block', 'display--flex');
+    header[0].classList.add('container');
+    for(var i=0; i<a_nav.length; i++){
+        a_nav[i].classList.remove('text--center');
+    }
+}
